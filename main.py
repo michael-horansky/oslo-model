b8287481a06cecca95b0f1750e68d4b60d5e2a5c
michael-horansky <michael.horansky@gmail.com>
2023-01-19 00:06:48 +0000

    task 1 completed

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 from oslo_lattice_class import *
 
 
+
+"""
+ALGORITHM TIME BENCHMARK TEST
+
+import time
+
+naive_start = time.time()
+kim = oslo_lattice(400)
+kim.simulate(1e3, 'never', 'naive')
+print("Naive: --- %s seconds ---" % (time.time() - naive_start))
+
+ceilidh_start = time.time()
+kim = oslo_lattice(400)
+kim.simulate(1e3, 'never', 'ceilidh')
+print("Ceilidh: --- %s seconds ---" % (time.time() - ceilidh_start))"""
+
+"""
 kim = oslo_lattice(10)
+
+
+my_h, my_z_th = kim.h.copy(), kim.z_th.copy()
 
 print(kim)
 
-kim.simulate(10, 'avalanche')
+random.seed(11)
+kim.simulate(100, 'never')
+print(kim)
+
+for i in range(1):
+    random.seed(10)
+    cur_lat = oslo_lattice(10, 0.5, my_h, my_z_th)
+    cur_lat.simulate(10, 'never', 'naive')
+    print(cur_lat)"""
+
+kim = oslo_lattice(16)
+chris = oslo_lattice(32)
+print(kim.aggregate_measurement(500, 10))
+print(chris.aggregate_measurement(5000, 10))
+
--- oslo_lattice_class.py
+++ oslo_lattice_class.py
@@ -24,6 +24,12 @@ class oslo_lattice():
     
     # --------- Constructor and descriptors ----------
     
+    def reset(self):
+        self.h = [0] * self.L
+        self.z_th = []
+        for i in range(self.L):
+            self.z_th.append(self.get_random_slope_threshold())
+    
     def __init__(self, L, p=0.5, heights = False, threshold_slopes = False):
         
         self.L = L
@@ -70,33 +76,67 @@ class oslo_lattice():
         
     def relaxation(self):
         s = 0
-        for i in range(self.L):
-            # check if avalanche continues
-            if self.z(i) > self.z_th[i]:
-                # avalanche continues. Increment the avalanche size
+        # The "ceilidh algorithm" is implemented here
+        cur_i = 0
+        max_i_affected = cur_i
+        
+        while(cur_i < self.L):
+            # check if site i is supercritical
+            if self.z(cur_i) > self.z_th[cur_i]:
+                # increment avalanche size
                 s += 1
-                # Relax site i
-                self.h[i] -= 1
-                if i != self.L - 1:
-                    self.h[i + 1] += 1
+                # relax current site
+                self.h[cur_i] -= 1
+                if cur_i != self.L - 1:
+                    self.h[cur_i + 1] += 1
                 # Change the threshold slope of the relaxed site
-                self.z_th[i] = self.get_random_slope_threshold()
+                self.z_th[cur_i] = self.get_random_slope_threshold()
+                # sites up to cur_i + 1 can be supercritical now. Update max_i_affected if needed
+                if max_i_affected < cur_i + 1:
+                    max_i_affected = cur_i + 1
+                # step back
+                cur_i = max(cur_i - 1, 0)
             else:
-                # If site i wasn't relaxed, sites with j>i won't be relaxed under
-                # any circumstances. Hence we can optimise by exiting the loop early
-                break
+                # step forward
+                cur_i += 1
+                # if cur_i > max_i_affected, we are outside of the range of possible supercriticality. exit.
+                if cur_i > max_i_affected:
+                    return(s)
+        return(s)
+        
+    def relaxation_naive(self):
+        s = 0
+        can_exit = False
+        while(not can_exit):
+            can_exit = True
+            for i in range(self.L):
+                # check if avalanche continues
+                if self.z(i) > self.z_th[i]:
+                    # avalanche continues. Increment the avalanche size
+                    s += 1
+                    # Relax site i
+                    self.h[i] -= 1
+                    if i != self.L - 1:
+                        self.h[i + 1] += 1
+                    # Change the threshold slope of the relaxed site
+                    self.z_th[i] = self.get_random_slope_threshold()
+                    # you have to do the loop again after this one finishes
+                    can_exit = False
         return(s)
     
-    def step(self):
+    def step(self, step_alg = 'ceilidh'):
         # Wrapper function for all the steps in one iteration of the algorithm
         # returns the avalanche size s associated with this step
         self.drive()
-        s = self.relaxation()
+        if step_alg == 'ceilidh':
+            s = self.relaxation()
+        if step_alg == 'naive':
+            s = self.relaxation_naive()
         return(s)
     
     # ------------ Operative functions -------------
     
-    def simulate(self, N_steps = 100, print_mode = 'avalanche'):
+    def simulate(self, N_steps = 100, print_mode = 'avalanche', step_alg = 'ceilidh'):
         # The main loop for a basic simulation
         t = 0
         printed_last_it = False
@@ -110,7 +150,7 @@ class oslo_lattice():
                     printed_last_it = True
                 else:
                     printed_last_it = False
-            cur_s = self.step()
+            cur_s = self.step(step_alg)
             t += 1
             if print_mode == 'avalanche' and printed_last_it:
                 print(f"------- t = {t}, s = {cur_s} --------")
@@ -118,5 +158,13 @@ class oslo_lattice():
             if print_mode == 'always':
                 print(f"------- t = {t} -------------")
                 print(self.print_lattice_state())
+    
+    def aggregate_measurement(self, N_steps = 100, N_repetitions = 10):
+        sum_pile_height = 0
+        for rep in range(N_repetitions):
+            self.reset()
+            self.simulate(N_steps, 'never')
+            sum_pile_height += self.h[0]
+        return(sum_pile_height / N_repetitions)
 
 
